57e5e637bb4faf541bb185d7f9df927f1fd39cdc
nielsero <niels.cpp@gmail.com>
2023-07-05 00:35:24 +0200

    Change hero title size on medium screens

--- src/components/layout/Hero.tsx
+++ src/components/layout/Hero.tsx
@@ -7,7 +7,9 @@ function Hero({ heroImage }: Props) {
     <section className="w-full px-4 py-4">
       <div className="container md:flex max-w-5xl mx-auto">
         <div className="md:w-[50%] text-center md:text-left">
-          <h1 className="font-bold mt-5 md:mt-20 text-6xl">Hero title</h1>
+          <h1 className="font-bold mt-5 md:mt-20 text-5xl sm:text-6xl lg:text-7xl">
+            Hero title
+          </h1>
           <p className="mt-3">Hero text</p>
         </div>
         <div className="text-center md:w-[50%]">
