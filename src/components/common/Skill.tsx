6a50133173f2c25ad126c701b5dc5aeb73a909e1
nielsero <niels.cpp@gmail.com>
2023-07-06 00:35:42 +0200

    Add tooltip to images to show on hover
    Used the title attribute

--- src/components/common/Skill.tsx
+++ src/components/common/Skill.tsx
@@ -3,7 +3,7 @@ type Props = { src: string; alt: string }
 function Skill({ src, alt }: Props) {
   return (
     <div className="w-[250px] flex justify-center items-center">
-      <img src={src} alt={alt} className="w-[90px] md:w-[100px]" />
+      <img src={src} alt={alt} title={alt} className="w-[90px] md:w-[100px]" />
     </div>
   )
 }
