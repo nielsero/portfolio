09f3efadecbd549a6e85de64f177a4574a3b3fb0
nielsero <niels.cpp@gmail.com>
2023-07-06 20:41:52 +0200

    Finish footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,9 @@
 export default function Footer() {
   return (
-    <footer id="footer" className="w-full p-4">
-      <div className="container max-w-5xl mx-auto"></div>
+    <footer id="footer" className="w-full p-5 bg-[#eeeeee]">
+      <div className="container max-w-5xl mx-auto text-center text-sky-800">
+        All rights reserved &copy; {new Date().getFullYear()} nielsero
+      </div>
     </footer>
   )
 }
