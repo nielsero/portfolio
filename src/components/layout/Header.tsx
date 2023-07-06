1f8c9ee2378943a43f89488bd69683d74959c9f8
nielsero <niels.cpp@gmail.com>
2023-07-06 20:36:41 +0200

    Change mobile header
    Make it span the entire screen.
    Added close button to it.

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -40,29 +40,29 @@ function Header({ logo, flag }: Props) {
           className="block md:hidden"
           onClick={() => setIsNavOpen(!isNavOpen)}
         >
-          {isNavOpen ? (
-            <AiOutlineClose size={20} />
-          ) : (
-            <AiOutlineMenu size={20} />
-          )}
+          <AiOutlineMenu size={20} />
         </div>
 
         <div
           className={
             isNavOpen
-              ? "fixed left-0 md:left-[-100%] top-0 w-[90%] ease-in-out duration-500 py-4 px-2 bg-white"
+              ? "fixed left-0 md:left-[-100%] top-0 w-full ease-in-out duration-500 py-4 px-2 bg-white"
               : "fixed left-[-100%] top-0 ease-in-out duration-500 py-4 px-2"
           }
         >
-          <div className="flex gap-2">
-            <img
-              src={logo}
-              alt="logo image of a black cat"
-              className="w-8 h-8 md:w-10 md:h-10"
-            />
-            <span className="font-bold text-2xl md:text-3xl">nielsero</span>
+          <div className="flex justify-between items-center">
+            <div className="flex gap-2">
+              <img
+                src={logo}
+                alt="logo image of a black cat"
+                className="w-8 h-8 md:w-10 md:h-10"
+              />
+              <span className="font-bold text-2xl md:text-3xl">nielsero</span>
+            </div>
+            <div onClick={() => setIsNavOpen(!isNavOpen)}>
+              <AiOutlineClose size={20} />
+            </div>
           </div>
-
           <nav className="flex flex-col pl-3 mt-4 gap-3">
             <Link href="#">About</Link>
             <Link href="#projects">Projects</Link>
